1777656e9963c5f343b04014c6b8373e09ffb43c
marcosfpr <mfprezende@gmail.com>
2022-03-19 20:09:46 -0300

    Examples

---
+++ examples/hello_example/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by mfpr on 3/19/22.
+//
+#include "boilerplate_library/format.hpp"
+
+int main(int argc, const char * argv[]) {
+    boilerplate::format::hello_world();
+    return 0;
+}
